28a11a6e4968b5902829b15879b56a3923c70e71
Alina_Em <alina@wavysys@com>
2020-11-29 21:13:06 +0200

    fixed

--- inspire-preloader.js
+++ inspire-preloader.js
@@ -1,1 +1,7 @@
-
+let mask=document.querySelector('.preloader');
+window.addEventListener('load', ()=> {
+    mask.classList.add('.done');
+    setTimeout(()=>{
+        mask.remove();
+    },600)
+} )
